3a167e2a0c146102b9528d549c7ec4cf0deeeef0
Thaju <93075984+Thajudecodes@users.noreply.github.com>
2022-02-10 07:47:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 #MIT License
 
-#Copyright (c) 2021 subinps
+#Copyright (c) 2022 [(Thajudheen](https://github.com/Thajudecodes/instagrm_bot)
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
